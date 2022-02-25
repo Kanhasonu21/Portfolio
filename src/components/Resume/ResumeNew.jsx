fcb542e48161a1bb464be50759fe8cde4551f3e1
Kanhasonu21 <kanhaiyak42@gmail.com>
2022-02-25 18:57:54 +0530

    Resume button name change

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -8,7 +8,6 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -23,7 +22,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button variant='primary' href={pdf} target='_blank'>
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
 
@@ -36,7 +35,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: 'center', position: 'relative' }}>
           <Button variant='primary' href={pdf} target='_blank'>
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Resume
           </Button>
         </Row>
       </Container>
